007ec5ca37b62262307fa7cb3100588111134d61
nomis51 <nomis51@users.noreply.github.com>
2025-02-01 15:51:43 -0500

    Remove field

--- backend/CoverMe.Backend/Models/JavaScript/Intellij/ProjectSettings.cs
+++ backend/CoverMe.Backend/Models/JavaScript/Intellij/ProjectSettings.cs
@@ -4,5 +4,4 @@ public class ProjectSettings
 {
     public required string ProjectRootPath { get; init; }
     public required string ChannelId { get; init; }
-    public required IntellijTheme Theme { get; init; }
 }
