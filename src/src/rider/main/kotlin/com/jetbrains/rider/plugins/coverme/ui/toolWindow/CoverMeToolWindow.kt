476a6620b7de87239be6afa84db9a394eb2eb233
nomis51 <nomis51@users.noreply.github.com>
2025-02-20 19:43:16 -0500

    (wip) menu button

--- src/src/rider/main/kotlin/com/jetbrains/rider/plugins/coverme/ui/toolWindow/CoverMeToolWindow.kt
+++ src/src/rider/main/kotlin/com/jetbrains/rider/plugins/coverme/ui/toolWindow/CoverMeToolWindow.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.rider.plugins.coverme.ui.toolWindow
 
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
@@ -9,35 +10,43 @@ import com.jetbrains.rider.plugins.coverme.models.coverage.CoverageOptions
 import com.jetbrains.rider.plugins.coverme.services.CoverageService
 import com.jetbrains.rider.plugins.coverme.ui.components.CoverageTable
 import com.jetbrains.rider.plugins.coverme.ui.components.IconButton
+import com.jetbrains.rider.plugins.coverme.ui.components.SettingsMenuButton
 import java.awt.BorderLayout
 import java.awt.FlowLayout
 import javax.swing.JPanel
 
 class CoverMeToolWindow(project: Project) {
     private val _coverageService: CoverageService = project.service<CoverageService>()
+
+    private val _testProjectsComboBox: ComboBox<String> = ComboBox<String>()
+    private val _coverageTable: CoverageTable = CoverageTable()
+    private val _filterTextfield: JBTextField = JBTextField().apply {
+        emptyText.text = "Type to filter..."
+    }
+    private val _runCoverageButton: IconButton = IconButton(AllIcons.RunConfigurations.TestState.Run).apply {
+        toolTipText = "Run coverage"
+    }
+    private val _buildAndRunCoverageButton: IconButton = IconButton(AllIcons.Run.Widget.Build).apply {
+        toolTipText = "Build and run coverage"
+    }
+    private val _settingsMenuButton: SettingsMenuButton = SettingsMenuButton(AllIcons.RunConfigurations.TestState.Red2)
+
     val content: JPanel = JPanel(BorderLayout())
 
     init {
         val topPanel = JPanel(BorderLayout())
         val toolbarPanel = JPanel(FlowLayout(FlowLayout.LEFT))
-        val testProjectSelect = ComboBox<String>()
-        toolbarPanel.add(testProjectSelect)
-
-        val runCoverageButton = IconButton(AllIcons.RunConfigurations.TestState.Run).apply {
-            toolTipText = "Run Coverage"
-        }
-        toolbarPanel.add(runCoverageButton)
+        toolbarPanel.add(_testProjectsComboBox)
+        toolbarPanel.add(_runCoverageButton)
+        toolbarPanel.add(_buildAndRunCoverageButton)
+        toolbarPanel.add(_settingsMenuButton)
 
         topPanel.add(
             toolbarPanel,
             BorderLayout.NORTH
         )
-
-        val txtFilter = JBTextField().apply {
-            emptyText.text = "Type to filter..."
-        }
         topPanel.add(
-            txtFilter,
+            _filterTextfield,
             BorderLayout.CENTER
         )
 
@@ -45,13 +54,13 @@ class CoverMeToolWindow(project: Project) {
             topPanel,
             BorderLayout.NORTH
         )
-
-        // table
-        val treeTable = CoverageTable()
         content.add(
-            treeTable.getComponent(),
+            _coverageTable.getComponent(),
             BorderLayout.CENTER
         )
+
+        retrieveTestProjects()
+        retrieveLastCoverage()
 
 //        val data = _coverageService.runCoverage(
 //            TestProject("C:\\Users\\nomis51\\GitHub\\CoverMe\\samples\\TestApp.Tests\\TestApp.Tests.csproj"),
@@ -60,12 +69,40 @@ class CoverMeToolWindow(project: Project) {
 //                ""
 //            )
 //        )
-        val data = _coverageService.parseLastCoverage(
-            CoverageOptions(
-                false,
-                ""
-            )
-        )
-        treeTable.updateData(data.toTypedArray())
+//        val data = _coverageService.parseLastCoverage(
+//            CoverageOptions(
+//                false,
+//                ""
+//            )
+//        )
+//        treeTable.updateData(data.toTypedArray())
+    }
+
+    private fun retrieveLastCoverage() {
+        ApplicationManager.getApplication()
+            .executeOnPooledThread {
+                val data = _coverageService.parseLastCoverage(
+                    CoverageOptions(
+                        false,
+                        _filterTextfield.text
+                    )
+                )
+                ApplicationManager.getApplication()
+                    .invokeLater {
+                        _coverageTable.updateData(data.toTypedArray())
+                    }
+            }
+    }
+
+    private fun retrieveTestProjects() {
+        ApplicationManager.getApplication()
+            .executeOnPooledThread {
+                val testProjects = _coverageService.getTestsProjects()
+                ApplicationManager.getApplication()
+                    .invokeLater {
+                        _testProjectsComboBox.removeAllItems()
+                        testProjects.forEach { _testProjectsComboBox.addItem(it.getName()) }
+                    }
+            }
     }
 }
